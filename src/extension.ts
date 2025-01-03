082fc15f32d4be86378833d587b825dd9256447f
Profpatsch <mail@profpatsch.de>
2025-01-03 18:41:08 +0100

    Bring up-to-date, directly call Viz from extension.ts, bump 0.1.0
    This simplifies the setup, I assume old versions of vscode still
    required a lot of workarounds.
    
    Move to new Viz library which is a real module, so we can directly
    call it from `extension.ts` instead of the workaround. This speeds up
    things considerably, because we don’t have to initialize the wasm
    library on each call, instead do it once on extension init.
    
    The `<pre style="all:unset">` trick is the same as the one used by
    the preview code generated by the mermaid extension.
    
    I removed the test stubs.
    I moved to npm lockfiles and removed yarn.
    
    This bumps the extension version to 0.1.0.

--- src/extension.ts
+++ src/extension.ts
@@ -2,28 +2,36 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as vizMod from '@viz-js/viz';
+import type MarkdownIt from 'markdown-it';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+    const viz = await vizMod.instance()
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "graphviz-markdown-preview" is now active!');
     return {
 
-        extendMarkdownIt(md) {
+        extendMarkdownIt(md: MarkdownIt) {
 
             const highlight = md.options.highlight;
 
-            md.options.highlight = (code, lang) => {
+            md.options.highlight = (code, lang, attrs) => {
 
                 if (lang && lang.match(/\bgraphviz\b/i)) {
-
-                    return `<div class="graphviz">${code}</div>`;
+                    try {
+                        return '<pre style="all:unset;">'
+                        + viz.renderString(code, { engine: 'dot', format: 'svg' })
+                        + '</pre>';
+                    } catch (e) {
+                        return `<pre><code>${e.message}</code></pre>`;
+                    }
                 }
 
-                return highlight(code, lang);
+                return highlight(code, lang, attrs);
 
             };
 
